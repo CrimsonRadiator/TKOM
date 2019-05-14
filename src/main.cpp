014858e87b140131e038164ce5cc899970c31cd3
Michał Sokólski <michsok@gmail.com>
2019-05-14 20:23:48 +0200

    Minor fixes in Node.h and main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,14 @@
 #include "json.h"
 #include "Source.h"
 #include "Scanner.h"
+#include "Parser.h"
 
 using json = nlohmann::json;
 
 int main() {
     Source src("a.txt");
     Scanner scanner(src);
-    while(src.getCurr()!=EOF) {
-        Token t = scanner.getNextToken();
-        std::cout << "TYPE: "+scanner.getTypeName(t)<< " DATA: "<<t.getText()<<"\n";
-    }
+    Parser parser(scanner);
 
 
     return 0;
